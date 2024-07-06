e0d5516f2506c9bac0d2fa792ab37c31b0beb932
Anamika Rai <130497329+AnamikaDEVOP@users.noreply.github.com>
2024-07-06 22:30:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -36,14 +36,16 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
+    #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base")
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    embeddings = HuggingFaceEmbeddings(model=model)
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     #llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", task="text2text-generation", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
